61f0bd640f37a93b4425f849bea5267e942c10d1
Boltovnya <bolto@tfwno.gf>
2014-04-13 00:52:20 +0100

    New client

--- mal2.py
+++ mal2.py
@@ -2,18 +2,22 @@ import variables, urllib2, json as simplejson, string, HTMLParser
 from xml.dom.minidom import parseString
 
 def mal2(send_data, msgarr, user):
+
+	
 	if len(msgarr) < 2 or 'hentai' in msgarr:
 		send_data("PRIVMSG %s :No or Bad seatch string." % variables.channel)
 	else:
-		file = urllib2.urlopen('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~%s' % msgarr)
+		url = 'http://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~' + msgarr[0]
+		file = urllib2.urlopen(url)
+	
 		data = file.read()
 		file.close()
 		dom = parseString(data)
-
-		xmlTag = dom.getElementsByTagName('id')[0].toxml()
-		xmlData = xmlTag.replace('<anime id=', '').replace('</anime>', '')
+		id = [elt.getAttribute('id') for elt in dom.getElementsByTagName('anime')]
+		
 
 		#print xmlTag		 #	For Testing ONLY
 		#print xmlData		 #	For Testing ONLY
-
-		send_data("PRIVMSG %s: http://www.animenewsnetwork.co.uk/encyclopedia/anime.php?id=%s" (variables.channel, id))
+		#id = str(id)
+		aniurl = 'http://www.animenewsnetwork.co.uk/encyclopedia/anime.php?id=' % id
+		send_data("PRIVMSG %s: %s" % (variables.channel, aniurl) )
