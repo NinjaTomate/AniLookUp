e29b50861cd6560385ced935516657ceb7dc8350
Boltovnya <bolto@tfwno.gf>
2014-04-12 22:25:27 +0100

    Hopefully Working

--- mal2.py
+++ mal2.py
@@ -5,13 +5,13 @@ def mal2(send_data, msgarr, user):
 	if len(msgarr) < 2 or 'hentai' in msgarr:
 		send_data("PRIVMSG %s :No or Bad seatch string." % variables.channel)
 	else:
-		file = urllib2.urlopen('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~' + msgarr)
+		file = urllib2.urlopen('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~%s' % msgarr)
 		data = file.read()
 		file.close()
 		dom = parseString(data)
 
 		xmlTag = dom.getElementsByTagName('id')[0].toxml()
-		xmlData = xmlTag.replace('<id>', '').replace('</id>', '')
+		xmlData = xmlTag.replace('<anime id=', '').replace('</anime>', '')
 
 		#print xmlTag		 #	For Testing ONLY
 		#print xmlData		 #	For Testing ONLY
