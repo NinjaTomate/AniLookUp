7734bec713ad076d8956294161f8252edc3d910f
Boltovnya <bolto@tfwno.gf>
2014-04-12 15:36:22 +0100

    Uploaded AniLookUp.py

---
+++ AniLookUp.py
@@ -0,0 +1,28 @@
+import variables, urllib2, urllib, string, HTMLParser
+from xml.dom.minidom import parseString
+
+def mal(send_data, msgarr, user):
+	if len(msgarr) < 2:
+		send_data("PRIVMSG %s :No or invalid search string supplied." % variables.channel)
+	else:
+		try:
+			parser = HTMLParser.HTMLParser()
+			query = string.join(msgarr)[3:]
+			getTag = dom.getElementsByTagName
+			url = "http://myanimelist.net/api/anime/search.xml?q=" % query
+			print query
+			file = urllib2.urlopen(url)
+			data = file.read()
+			file.close()
+			dom = parseString(data)
+			xmlID = getTag('id')
+			xmlName = getTag('title')
+			xmlType = getTag('type')
+			xmlStatus = getTag('status')
+			ID = xmlTag.replace('<id>','').replace('</id>','')
+			Name = xmlTag.replace('<title>','').replace('</title>','')
+			Type = xmlTag.replace('<type>','').replace('</type>','')
+			Status = xmlTag.replace('<status>','').replace('</status>','')
+			send_data("PRIVMSG %s :", Name, "http://myanimlist.net/anime/" % (ID, Type, Status  variables.channel))
+		except:
+			send_data("PRIVMSG %s : No anime found by that name, or incorrect name entered" % variables.channel)
