9f29da6e124d556692fb372162cf65bce4d5ac40
Boltovnya <bolto@tfwno.gf>
2014-04-12 22:00:43 +0100

    New, hopefully working module

---
+++ mal2.py
@@ -0,0 +1,19 @@
+import variables, urllib2, json as simplejson, string, HTMLParser
+from xml.dom.minidom import parseString
+
+def mal2(send_data, msgarr, user):
+	if len(msgarr) < 2 or 'hentai' in msgarr:
+		send_data("PRIVMSG %s :No or Bad seatch string." % variables.channel)
+	else:
+		file = urllib2.urlopen('http://cdn.animenewsnetwork.com/encyclopedia/api.xml?title=~' + msgarr)
+		data = file.read()
+		file.close()
+		dom = parseString(data)
+
+		xmlTag = dom.getElementsByTagName('id')[0].toxml()
+		xmlData = xmlTag.replace('<id>', '').replace('</id>', '')
+
+		#print xmlTag		 #	For Testing ONLY
+		#print xmlData		 #	For Testing ONLY
+
+		send_data("PRIVMSG %s: http://www.animenewsnetwork.co.uk/encyclopedia/anime.php?id=%s" (variables.channel, id))
